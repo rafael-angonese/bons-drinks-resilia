20843884c5c164316c182ad0b3c99fa4900ed700
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-08 14:42:40 -0300

    :sparkles: feat: created form with jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import Button from "./components/Button";
+
 function App() {
-  return <div>Hello World</div>
+  const meuInput = {
+    width: "100%",
+    padding: "12px 20px",
+    marginBottom: "20px",
+  };
+  const meuBotao = {
+    width: "80px",
+    padding: "5px 20px",
+    textAlign: "center",
+    fontWeight: "bold",
+    backgroundColor: "#ddd255",
+    border: "1px solid #cccccc",
+    marginRight: "15px",
+  };
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Olá, mundo!</h1>
+
+      <p style={{ color: "green", textAlign: "center" }}>
+        Meu primeiro form com JSX
+      </p>
+
+      <form style={{ width: "400px", margin: "0 auto" }}>
+        <label>Nome</label>
+        <input style={meuInput} />
+        <label>Email</label>
+        <input style={meuInput} />
+        <label>Telefone</label>
+        <input style={meuInput} />
+        <button style={meuBotao}>Enviar</button>
+
+        <Button />
+      </form>
+    </div>
+  );
 }
 
 export default App;
