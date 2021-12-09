4cf564597a9d0b60d042db1ce38ddba50218bc63
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-08 22:06:38 -0300

    :sparkles: feat: included js inline

--- src/App.js
+++ src/App.js
@@ -16,8 +16,55 @@ function App() {
     marginRight: "15px",
   };
 
+  const clienteJoao = {
+    nome: "João",
+    idade: 37,
+    compras: [
+      { nome: "Notebook", preco: "2500" },
+      { nome: 'Motocicleta"', preco: "10000" },
+      { nome: "Smartphone", preco: "1500" },
+      { nome: "Conjunto de panelas", preco: "2000" },
+    ],
+    cidade: "Belém",
+  };
+
+  const clienteMaria = {
+    nome: "Maria",
+    idade: 18,
+    compras: [
+      { nome: "Computador", preco: "2500" },
+      { nome: "Coletânia de livros", preco: "1000" },
+      { nome: "Smartphone", preco: "1500" },
+    ],
+    cidade: "Rio de Janeiro",
+  };
+
+  const checkIdade = (idade) => {
+    if (idade >= 18) {
+      return (
+        <div>
+          <p>Maior de idade: {idade}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Menor de idade: {idade}</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
+      {/*Utilizamos as chaves para poder passar as variaveis pro JSX*/}
+
+      <p>{clienteJoao.nome}</p>
+
+      <p>{clienteJoao.idade >= 18 ? "Maior de idade" : "Menor de idade"}</p>
+
+      <p> {checkIdade(clienteJoao.idade)} </p>
+
       <h1 style={{ textAlign: "center" }}>Olá, mundo!</h1>
 
       <p style={{ color: "green", textAlign: "center" }}>
