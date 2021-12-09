be556f11c7a4c75fdcbb9184fe561b0e828ed93c
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-08 22:22:03 -0300

    :sparkles: chore: changed to ContactForm component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import Button from "./components/Button";
 import ButtonClass from "./components/ButtonClass";
+import ConctactForm from "./components/ContactForm";
 
 function App() {
-  const meuInput = {
-    width: "100%",
-    padding: "12px 20px",
-    marginBottom: "20px",
-  };
-  const meuBotao = {
-    width: "80px",
-    padding: "5px 20px",
-    textAlign: "center",
-    fontWeight: "bold",
-    backgroundColor: "#ddd255",
-    border: "1px solid #cccccc",
-    marginRight: "15px",
-  };
 
   const clienteJoao = {
     nome: "João",
@@ -101,23 +88,7 @@ function App() {
         </ul>
       </div>
 
-      <h1 style={{ textAlign: "center" }}>Olá, mundo!</h1>
-
-      <p style={{ color: "green", textAlign: "center" }}>
-        Meu primeiro form com JSX
-      </p>
-
-      <form style={{ width: "400px", margin: "0 auto" }}>
-        <label>Nome</label>
-        <input style={meuInput} />
-        <label>Email</label>
-        <input style={meuInput} />
-        <label>Telefone</label>
-        <input style={meuInput} />
-        <button style={meuBotao}>Enviar</button>
-        <Button />
-        <ButtonClass />
-      </form>
+      <ConctactForm />
     </div>
   );
 }
