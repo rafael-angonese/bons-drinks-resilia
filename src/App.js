c18855fbe8a9b3e60569921cb3ee0ce05e342fe8
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-13 19:49:31 -0300

    :coffin: chore: removed css grid layout

--- src/App.js
+++ src/App.js
@@ -8,29 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid">
-      <div className="box item1">One</div>
-      <div className="box item2">Two</div>
-
-      <div className="box item3">
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        <div className="box1">Content</div>
-        {/* <div className="box1">Content</div> */}
-       
-      </div>
-
-      <div className="box item4">Four</div>
-      <div className="box item5">Five</div>
-      <div className="box item6">Six</div>
-      <div className="box item7">Seven</div>
-      <div className="box item8">Eight</div>
-      <div className="box item9">Nine</div>
-    </div>
+    
   );
 }
 
