9cc2bf3471ac2d6ca68db3c76c50e45f6bf97e36
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-13 18:18:21 -0300

    :sparkles: feat: included layout init flexbox

--- src/App.js
+++ src/App.js
@@ -8,13 +8,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid">
-     
-      <header> <Header /></header>
-      <main>Main</main>
-      <aside>aside</aside>
-      <footer>Fotter</footer>
-    </div>
+    <main className="flexGrid">
+    <section className="topo"></section>
+    <section className="meio">
+      {/* primeira linha do flex-grid */}
+      {/* é muito comum também usarem uma div class = row para segurar todos os itens de coluna daquela linha. 
+      Como aqui o exemplo é mais simples, não fiz assim, mas paro algo mais robusto seria o ideal*/}
+      <div className="bloco4colunas linha"></div>
+      <div className="bloco6colunas linha"></div>
+      <div className="bloco2colunas linha"></div>
+      {/* segunda linha  do flex-grid */}
+      <div className="bloco2colunas"></div>
+      <div className="bloco4colunas"></div>
+      <div className="bloco4colunas"></div>
+      <div className="bloco2colunas"></div>
+      {/* terceira linha  do flex-grid */}
+      <div className="bloco6colunas"></div>
+      <div className="bloco2colunas"></div>
+      <div className="bloco4colunas"></div>
+    </section>
+    <section className="fim"></section>
+  </main>
   );
 }
 
