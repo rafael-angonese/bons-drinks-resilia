ac37c4179d7906a27b73b5abeda29e670753e59f
Rafael Angonese <rafael.angonese09@gmail.com>
2021-12-20 20:02:39 -0300

    :sparkles: chore: fetch api drinks

--- src/components/Drinks/Drinks.js
+++ src/components/Drinks/Drinks.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "../Button/Button";
 
 class Drinks extends React.Component {
   constructor(props) {
@@ -7,23 +6,49 @@ class Drinks extends React.Component {
 
     this.state = {
       loading: true,
+      dados: [],
     };
+
+    //url que vamos usar para o fetch dos dados
+    this.url =
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
   }
 
   componentDidMount() {
-    //fetch('url').then(())
-    console.log("did mount");
-    this.setState({
-      loading: false,
-    });
+    fetch(this.url)
+      .then((resposta) => resposta.json())
+      .then((json) => {
+
+        console.log(json)
+        // após ter os dados da API, precisamos guardá-los em um estado para que ao estado ser atualizado, 
+        // o React gere uma nova renderização do componente
+        // precisamos ver também como é o retorno do servidor, 
+        // aqui precisamos acessar o objeto dados para ter o array com os drinks
+        return this.setState({ loading: false, dados: json.drinks });
+      });
   }
 
   render() {
-    console.log(this.state.loading);
     return (
       <>
+        <h1>Drinks</h1>
         {this.state.loading && <>Carregando...</>}
         {this.state.loading ? "Carregando..." : ""}
+
+        <ul>
+          {/*vamos usar o método de array map (pois a resposta que temos no estado é um array de objetos) 
+          e vamos iterar sobre ele retornando uma li com o nome do drink 
+          - OBS.: quando temos elementos HTML sendo gerados com map ou outros iteradores é necessário ter um atributo 
+          "key" nesse elemento. Esse atributo "key" tem que ser um identificador único para que o react 
+          saiba qual elemento na árvore do DOM ele é. 
+          No nosso caso estamos usando o próprio id dos Drinks como esse key,
+          mas poderia ser também o argumento index do map*/}
+          {this.state.dados.map((item) => {
+            return <li key={item.idDrink}>
+                {item.strDrink}
+                </li>;
+          })}
+        </ul>
       </>
     );
   }
