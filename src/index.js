aa595f6a51f77c386faf5a112ea05e6a5b4ad2bd
Rafael Angonese <rafael.angonese09@gmail.com>
2022-01-10 19:36:22 -0300

    :sparkles: feat: provided UserContext

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { UserContextProvider } from "./contexts/userContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
