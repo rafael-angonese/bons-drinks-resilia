a5291ec8a5663cd0a4ea127fa0ab8ca0bbdec3ac
Rafael Angonese <rafael.angonese09@gmail.com>
2022-01-10 19:37:21 -0300

    :recycle: refactor: refactor Header component to funcional and get userContext value

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useContext } from "react/cjs/react.development";
+import UserContext from "../../contexts/userContext";
 
 import styles from "./styles.module.css";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className={styles.navMenu}>
-        <nav className={styles.myNav}>
-          <Link className={styles.menuItem} to="/drinks">
-            Drinks
-          </Link>
-          <Link className={styles.menuItem} to="/drinksFunk">
-            Drinks Func
-          </Link>
-          <Link className={styles.menuItem} to="/time">
-            Sobre Nós
-          </Link>
-          <Link className={styles.logo} to="/">
-            Bons Drinks
-          </Link>
-          <Link className={styles.menuItem} to="/time">
-            Nosso Time
-          </Link>
-          <Link className={styles.menuItem} to="/contact">
-            Contato
-          </Link>
-        </nav>
-      </header>
-    );
-  }
+function Header() {
+  const { name } = useContext(UserContext);
+
+  return (
+    <header className={styles.navMenu}>
+      <nav className={styles.myNav}>
+        <Link className={styles.menuItem} to="/drinks">
+          Drinks
+        </Link>
+        <Link className={styles.menuItem} to="/drinksFunk">
+          Drinks Func
+        </Link>
+        <Link className={styles.menuItem} to="/time">
+          Sobre Nós
+        </Link>
+        <Link className={styles.logo} to="/">
+          Bons Drinks
+        </Link>
+        <Link className={styles.menuItem} to="/time">
+          Nosso Time
+        </Link>
+        <Link className={styles.menuItem} to="/contact">
+          Contato
+        </Link>
+        <p>Usuario: {name}</p>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
